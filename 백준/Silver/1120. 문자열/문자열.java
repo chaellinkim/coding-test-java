11283c40e9b5a302713ba986687c7a77497cb4ab
clin0927 <91306706+clin0927@users.noreply.github.com>
2023-02-23 23:52:05 +0900

    [Silver IV] Title: 문자열, Time: 124 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        //Scanner sc = new Scanner(System.in);
+        //String str1 = sc.next();
+        //String str2 = sc.next();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        String str1=input[0];
+        String str2=input[1];
+        
+        
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= str2.length() - str1.length(); i++) {
+            int cnt = 0;
+            for (int j = 0; j < str1.length(); j++) {
+                if (str1.charAt(j) != str2.charAt(j + i)) {
+                    cnt++;
+                }
+            }
+            if (min > cnt) {
+                min = cnt;
+            }
+        }
+
+        System.out.println(min);
+    }
+}
