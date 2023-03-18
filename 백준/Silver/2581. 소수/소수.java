f7c9ccb046157d675e28b0db96a328916b067f7f
clin0927 <91306706+clin0927@users.noreply.github.com>
2023-03-18 17:05:24 +0900

    [Silver V] Title: 소수, Time: 132 ms, Memory: 14504 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.java
+++ 백준/Silver/2581. 소수/소수.java
@@ -5,13 +5,15 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static boolean[] prime;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int M = Integer.parseInt(br.readLine());
         int N = Integer.parseInt(br.readLine());
-        int sum = 0, min = 0;
+        int sum = 0, min = 0, sum2 = 0, min2 = 0;
 
-        for (int i = M; i <= N; i++) {
+        /*for (int i = M; i <= N; i++) {
             for (int j = 2; j <= i; j++) {
                 if (i == j) {
                     sum += i;
@@ -29,6 +31,34 @@ public class Main {
             System.out.println(-1);
         } else {
             System.out.printf("%d\n%d", sum, min);
+        }*/
+
+        makePrime(N);
+
+        for (int i = M; i <= N; i++) {
+            if (prime[i] == false) {
+                sum2 += i;
+                if (min2 == 0) {
+                    min2 = i;
+                }
+            }
+        }
+        if (sum2 == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.printf("%d\n%d\n", sum2, min2);
+        }
+
+    }
+
+    public static void makePrime(int N) {
+        prime = new boolean[N + 1];
+        prime[0] = true;
+        prime[1] = true;
+        for (int i = 2; i < Math.sqrt(N); i++) {
+            for (int j = i * i; j < prime.length; j += i) {
+                prime[j] = true;
+            }
         }
     }
 }
