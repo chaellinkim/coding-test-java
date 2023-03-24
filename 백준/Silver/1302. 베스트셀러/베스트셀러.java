630c1a2e5fbaabb6ddadec88ee6d05336d99e099
clin0927 <91306706+clin0927@users.noreply.github.com>
2023-03-24 17:08:10 +0900

    [Silver IV] Title: 베스트셀러, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -1,36 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int value,max=0;
+        int max=0;
         Map<String, Integer> map = new HashMap<>();
-        String str2 = null;
+        List<String> list = new ArrayList<>();
 
         for(int i=0; i<n; i++){
             String str=br.readLine();
-            value=map.getOrDefault(str,0)+1;
-            map.put(str,value);
-            if(max<=value){
-                if(max==value){
-                    if(str2.compareTo(str)>0){
-                        str2 = str;
-                    }
-                }else {
-                    max = value;
-                    str2 = str;
-                }
+            map.put(str,map.getOrDefault(str,0)+1);
+            max = Math.max(max,map.get(str));
+        }
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            if(entry.getValue()==max) {
+                list.add(entry.getKey());
             }
         }
-        System.out.println(str2);
-        /*System.out.println("출력");
-        for(Map.Entry<String,Integer> pair : map.entrySet()){
-            System.out.println(pair.getKey()+" "+pair.getValue());
-        }*/
+        Collections.sort(list);
+        System.out.println(list.get(0));
+
     }
 }
